c162a594fe996b9a10be6992992bbe23e141362c
Alisson Martineli <martineli.alisson@gmail.com>
2022-10-22 20:00:32 -0300

    feat: crud order

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,8 +1,10 @@
 import Order from "../../../../domain/checkout/entity/order";
+import OrderItem from "../../../../domain/checkout/entity/order_item";
+import OrderRepositoryInterface from "../../../../domain/checkout/repository/order-repository.interface";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
@@ -21,5 +23,73 @@ export default class OrderRepository {
         include: [{ model: OrderItemModel }],
       }
     );
+  }
+
+  async update(entity: Order): Promise<void> {
+    await OrderItemModel.destroy({
+      where: {
+        order_id: entity.id,
+      }
+    })
+
+    entity.items.forEach(async (item) => {
+      await OrderItemModel.create({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          product_id: item.productId,
+          quantity: item.quantity,
+          order_id: entity.id
+      })
+    })
+  }
+
+  async find(id: string): Promise<Order> {
+    try {
+      const orderModel = await OrderModel.findOne({
+        where: {
+          id,
+        },
+        rejectOnEmpty: true,
+        include: [{ model: OrderItemModel }],
+      });
+
+      const items: OrderItem[] = orderModel.items.map(
+        itemModel => new OrderItem(
+          itemModel.id,
+          itemModel.name,
+          itemModel.price,
+          itemModel.product_id,
+          itemModel.quantity
+        )
+      )
+
+      const order = new Order(id, orderModel.customer_id, items)
+
+      return order
+    } catch (error) {
+      throw new Error("Order not found");
+    }
+  }
+
+  async findAll(): Promise<Order[]> {
+    const ordersModels = await OrderModel.findAll({
+      include: [{ model: OrderItemModel }],
+    })
+
+    const orders = ordersModels.map(orderModel => {
+      const items = orderModel.items.map(
+        itemModel => new OrderItem(
+          itemModel.id,
+          itemModel.name,
+          itemModel.price,
+          itemModel.product_id,
+          itemModel.quantity
+        )
+      )
+      return new Order(orderModel.id, orderModel.customer_id, items)
+    })
+
+    return orders
   }
 }
